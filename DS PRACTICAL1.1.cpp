402330efde634737707d07f1471eca77cffca502
Ogweno <84031459+Ogweno-D@users.noreply.github.com>
2022-03-17 23:50:25 +0300

    Update DS PRACTICAL1.1.cpp

--- DS PRACTICAL1.1.cpp
+++ DS PRACTICAL1.1.cpp
@@ -12,6 +12,7 @@ int main()
 	int array[]={23,14,15,26,23};
 	j=n;
 	
+		//This program displays the total number of elements originally with their repective indexes.
 		for(i=0;i<n;i++)
 			{
 		printf("The array[%d] elements are %d:\n",i,array[i]);
@@ -20,26 +21,28 @@ int main()
 			n=n+1;
 		
 		while(j>=n){
-			array[j+1]=array[j];
-			j=j-1;
-		}
+		     array[j+1]=array[j];
+		     j=j-1;
+		   }
+	
 		array[j]=item;
 		
 		printf("After insertion the elements of the array are:\n");
-		
+	
 		for(i=0;i<n;i++){
 			printf("Array[%d]=%d\n",i, array[i]);
 			
 		}
 	
-	printf("=====================")
 	
-	printf("Deletion") 
+	printf("=====================");
+	
+	printf("Deletion") ;
 		
 	printf("When you delete:\n");
 	
-	j=k;
-	k=3;
+		j=k;
+		k=3;
 	
 		while(j<n){
 			array[j-1]= array[j];
@@ -50,9 +53,7 @@ int main()
 		//Prints the array after deletion.
 		for(i=0;i<n;i++){
 			printf("Array[%d]=%d\n",i, array[i]);
-			
-		}
-		
+		}	
 		
 	printf("Now when you want to update a new value to that position:\n");
 	
